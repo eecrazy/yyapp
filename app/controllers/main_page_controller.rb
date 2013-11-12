9570cbf87d3ac59fccb0979ec9d92c3f0c0f8d2c
cnmpp902 <cnmpp902@gmail.com>
2013-11-12 12:27:26 +0000

    ddd

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,10 +1,11 @@
 class MainPagesController < ApplicationController
   def home
-    if user_signed_in?
-    @apps = App.limit(5).order("updated_at desc")
-    else
-      render 'sign'
-    end    
+    # if user_signed_in?
+    # @apps = App.limit(5).order("updated_at desc")
+    # else
+    #   render 'sign'
+    # end 
+    
   end
   
   def index
