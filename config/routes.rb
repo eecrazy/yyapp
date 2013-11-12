800522f81044790f4a3cb97b4391868a84db56ac
cnmpp902 <cnmpp902@gmail.com>
2013-11-12 12:23:14 +0000

    ddd

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Yyapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'main_pages#home'
-  resource :main_page
-  #root to: 'main_pages#home'  
+  #root to: 'main_pages#home'
+  match '/home', to: 'main_pages#home', via:'get'
   match '/index', to: 'main_pages#index', via:'get'
   match '/sign', to: 'main_pages#sign', via:'get'
 
