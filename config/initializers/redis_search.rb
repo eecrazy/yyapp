8350f5f5a9efbaa5589c038d419d3c12db1f78b6
cnmpp902 <cnmpp902@gmail.com>
2013-12-06 13:36:02 +0000

    Add precompiled assets for Heroku

--- config/initializers/redis_search.rb
+++ config/initializers/redis_search.rb
@@ -1,13 +1,13 @@
-require "redis"
-require "redis-namespace"
-require "redis-search"
-App.new
-redis = Redis.new(:host => Setting.host ,:port => "6379")
-redis.select(3)
-# don't forget change the namespace
-redis = Redis::Namespace.new("yyapp:redis_search", :redis => redis)
-Redis::Search.configure do |config|
-  config.redis = redis
-  config.complete_max_length = 50
-  config.pinyin_match = true
-end
+# require "redis"
+# require "redis-namespace"
+# require "redis-search"
+# App.new
+# redis = Redis.new(:host => Setting.host ,:port => "6379")
+# redis.select(3)
+# # don't forget change the namespace
+# redis = Redis::Namespace.new("yyapp:redis_search", :redis => redis)
+# Redis::Search.configure do |config|
+#   config.redis = redis
+#   config.complete_max_length = 50
+#   config.pinyin_match = true
+# end
